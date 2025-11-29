e9e866954383f1d1e12508e2944eec2af9a30bf4
Jan Mewes <jan.mewes@experimental-software.com>
2025-11-29 08:50:10 +0100

    Reactivate module test!

--- src/test/java/com/experimental_software/spring_adventures/ModuleVerificationsTest.java
+++ src/test/java/com/experimental_software/spring_adventures/ModuleVerificationsTest.java
@@ -1,13 +1,11 @@
 package com.experimental_software.spring_adventures;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.modulith.core.ApplicationModules;
 
 /**
  * @see <a href="https://youtu.be/05x0_1y9l_8?si=vPvkyk72aXBhZ9c4">youtube.com</a>
  */
-@Disabled
 class ModuleVerificationsTest {
 
     @Test
