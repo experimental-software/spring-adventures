ccd5e4e28f265fc8db619745af36fbdb28f42ece
Jan Mewes <jan.mewes@experimental-software.com>
2025-11-29 08:50:49 +0100

    chore: add compiler plugin for null safety
    GH-10

--- src/main/java/com/experimental_software/spring_adventures/production/ProductDTO.java
+++ src/main/java/com/experimental_software/spring_adventures/production/ProductDTO.java
@@ -1,14 +1,34 @@
 package com.experimental_software.spring_adventures.production;
 
+import org.jspecify.annotations.NullMarked;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 
+@Builder
+@NullMarked
 public record ProductDTO(
     @Schema(example = "2")
     @JsonProperty("_id")
     int id,
 
     @Schema(example = "Bearing Ball")
+    //@NonNull
     String name) {
+
+    public static ProductDTO noName() {
+        return ProductDTO.builder()
+            .id(42)
+            .name("n/a")
+            .build();
+
+        /*
+        return ProductDTO.builder()
+            .id(42)
+            .name(null) // yields compiler error: [NullAway] passing @Nullable parameter 'null' where @NonNull is required
+            .build();
+         */
+    }
 }
---
+++ src/test/java/com/experimental_software/spring_adventures/production/ProductDTOTest.java
@@ -0,0 +1,16 @@
+package com.experimental_software.spring_adventures.production;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ProductDTOTest {
+
+    @Test
+    @DisplayName("should create product without name")
+    void test_noName_happy_path() {
+        var result = ProductDTO.noName();
+        assertThat(result.name()).isEqualTo("n/a");
+    }
+}
